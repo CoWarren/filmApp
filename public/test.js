178e7b27b5c8d67cb776ce2dee0b649d493231d8
Chris-Warren <35700610+CoWarren@users.noreply.github.com>
2022-11-23 00:17:43 +0000

    Update test.js

--- public/test.js
+++ public/test.js
@@ -1,7 +1,7 @@
 
 async function getmovie() {
 
-  const URL = `https://api.themoviedb.org/3/discover/movie?api_key=faac618f8b55fe67036720b29d0f430d&language=en-US&sort_by=release_date.asc&include_adult=false&include_video=false&page=1&`
+  const URL = `https://api.themoviedb.org/3/discover/movie?api_key=(APIKEY)&language=en-US&sort_by=release_date.asc&include_adult=false&include_video=false&page=1&`
   const result = await fetch(`${URL}`)
   const data = await result.json() 
   console.table(data)
